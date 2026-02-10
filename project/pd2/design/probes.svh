6e12f4191d32e926a849c40ef5e97de9368809c1
DanielFer21 <ferlisidaniel@gmail.com>
2026-02-09 21:20:55 -0500

    Probes fixed

--- project/pd2/design/probes.svh
+++ project/pd2/design/probes.svh
@@ -1,16 +1,16 @@
 // ----  Probes  ----
-`define PROBE_F_PC pc
-`define PROBE_F_INSN insn
+`define PROBE_F_PC pc_fetch
+`define PROBE_F_INSN insn_fetch
 
-`define PROBE_D_PC        pc_decode 
-`define PROBE_D_OPCODE    opcode 
-`define PROBE_D_RD        rd
-`define PROBE_D_FUNCT3    funct3
-`define PROBE_D_RS1       rs1 
-`define PROBE_D_RS2       rs2 
-`define PROBE_D_FUNCT7    funct7 
-`define PROBE_D_IMM       imm 
-`define PROBE_D_SHAMT     shamt 
+`define PROBE_D_PC pc_decode 
+`define PROBE_D_OPCODE opcode 
+`define PROBE_D_RD rd
+`define PROBE_D_FUNCT3 funct3
+`define PROBE_D_RS1 rs1 
+`define PROBE_D_RS2 rs2 
+`define PROBE_D_FUNCT7 funct7 
+`define PROBE_D_IMM imm 
+`define PROBE_D_SHAMT shamt 
 // ----  Probes  ----
 
 // ----  Top module  ----
